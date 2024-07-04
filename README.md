47ac9f7b6bca7c55356408420cc7503464666f4d
venu <venunaik737@gmail.com>
2024-07-04 13:25:26 +0530

    code added

---
+++ README.md
@@ -0,0 +1,21 @@
+## A Paytm like application were users can send or receive and perform transactions on dummy money.
+
+## Signup
+
+![Signup Page](/frontend/images/Signup.png)
+
+## Signin
+
+![Signin Page](</frontend/images/Screenshot%20(116).png>)
+
+## Dashboard
+
+![Signup Page](</frontend/images/Screenshot%20(114).png>)
+
+## Send Money
+
+![SendMoney Page](</frontend/images/Screenshot%20(117).png>)
+
+## Users Transactions and balance
+
+![Users](</frontend/images/Screenshot%20(112).png>)
---
+++ backend/db.js
@@ -0,0 +1,58 @@
+// backend/db.js
+const mongoose = require("mongoose");
+require("dotenv").config(); // Load .env file
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to User model
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Account = mongoose.model("Account", accountSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+  Account,
+};
